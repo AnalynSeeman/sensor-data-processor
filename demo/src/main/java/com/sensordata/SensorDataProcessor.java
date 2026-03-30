52c0d07d4af3027cbdd38006032ac6e18607b17f
Analyn Seeman <analynseeman@gmail.com>
2026-03-30 15:00:25 -0500

    ai optimizations

--- demo/src/main/java/com/sensordata/SensorDataProcessor.java
+++ demo/src/main/java/com/sensordata/SensorDataProcessor.java
@@ -17,13 +17,12 @@ public class SensorDataProcessor{
 
     // calculates average of sensor data
     private double average(double[] array) {
-        int i = 0;
-        double val = 0;
-        for (i = 0; i < array.length; i++) {
-            val += array[i];
+        double sum = 0;
+        for (double value : array) {
+            sum += value;
         }
 
-        return val / array.length;
+        return sum / array.length;
     }
 
     // calculate data
@@ -31,27 +30,38 @@ public class SensorDataProcessor{
 
         long startTime = System.nanoTime();
 
-        int i, j, k = 0;
+        int i, j, k;
         double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
 
-        BufferedWriter out;
-
         // Write racing stats data into a file
-        try {
-            out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
+        try (BufferedWriter out = new BufferedWriter(new FileWriter("RacingStatsData.txt"))) {
 
             for (i = 0; i < data.length; i++) {
                 for (j = 0; j < data[0].length; j++) {
-                    for (k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+                    double[] dataRow = data[i][j];
+                    double[] data2Row = data2[i][j];
+                    double limitSquared = Math.pow(limit[i][j], 2.0);
+                    double dataRowAverage = average(dataRow);
+                    double data2RowSum = 0;
 
-                        if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
+                    for (k = 0; k < data[0][0].length; k++) {
+                        double dataVal = dataRow[k];
+                        double data2Val = dataVal / d - limitSquared;
+                        data2Row[k] = data2Val;
+                        data2RowSum += data2Val;
+
+                        double data2RowAverage = data2RowSum / data2Row.length;
+
+                        if (data2RowAverage > 10 && data2RowAverage < 50)
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+                        else if (Math.max(dataVal, data2Val) > dataVal)
                             break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
-                                && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
-                            data2[i][j][k] *= 2;
+                        else if (Math.pow(Math.abs(dataVal), 3) < Math.pow(Math.abs(data2Val), 3)
+                                && dataRowAverage < data2Val && (i + 1) * (j + 1) > 0) {
+                            data2Val *= 2;
+                            data2Row[k] = data2Val;
+                            data2RowSum += data2Val / 2;
+                        }
                         else
                             continue;
                     }
@@ -63,8 +73,6 @@ public class SensorDataProcessor{
                     out.write(data2[i][j] + "\t");
                 }
             }
-
-            out.close();
 
             long endTime = System.nanoTime();
             long elapsedMs = (endTime - startTime) / 1_000_000;
