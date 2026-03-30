a8bcc78179b93509471b41681fd1ec005e87cb31
Analyn Seeman <analynseeman@gmail.com>
2026-03-30 14:25:29 -0500

    maunal changes

--- demo/src/main/java/com/sensordata/SensorDataProcessor.java
+++ demo/src/main/java/com/sensordata/SensorDataProcessor.java
@@ -43,26 +43,31 @@ public class SensorDataProcessor{
             for (i = 0; i < data.length; i++) {
                 for (j = 0; j < data[0].length; j++) {
                     for (k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+                        // data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+                        data2[i][j][k] = data[i][j][k] / d - (limit[i][j] * limit[i][j]);
 
                         if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
                             break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+                        // else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+                        else if (data2[i][j][k] > currentData)
                             break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                        // else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                        else if (Math.abs(currentData) < Math.abs(data2[i][j][k]) && average(data[i][j]) < data2[i][j][k])
                                 && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
-                            data2[i][j][k] *= 2;
+                            // data2[i][j][k] *= 2;
+                            data2[i][j][k] += 2;
                         else
                             continue;
                     }
-                }
-            }
-
-            for (i = 0; i < data2.length; i++) {
-                for (j = 0; j < data2[0].length; j++) {
                     out.write(data2[i][j] + "\t");
                 }
             }
+
+            // for (i = 0; i < data2.length; i++) {
+            //     for (j = 0; j < data2[0].length; j++) {
+            //         out.write(data2[i][j] + "\t");
+            //     }
+            // }
 
             out.close();
 
